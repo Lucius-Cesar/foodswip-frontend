d7d77bbaf75f7d115a036e86bea2dab7137f63ae
lucius-César <luciendelgrange5@gmail.com>
2023-12-29 18:07:01 +0100

    checkout form validation

--- src/app/checkout/page.js
+++ src/app/checkout/page.js
@@ -1,5 +1,6 @@
 "use client";
 
+import { useRouter } from "next/navigation";
 import { useSelector } from "react-redux";
 import { useState, useEffect } from "react";
 
@@ -7,13 +8,33 @@ import Cart from "../../components/eaterView/Cart";
 import RestaurantLogo from "../../components/RestaurantLogo";
 import OrderTabBtn from "@/components/eaterView/OrderTabBtn";
 import FormInput from "../../components/ui/FormInput";
-import { ExclamationCircleIcon, EnvelopeIcon } from "@heroicons/react/20/solid";
+import DefaultBtn from "@/components/ui/DefaultBtn";
 
 export default function Checkout() {
+  const router = useRouter();
+
   const restaurant = useSelector((state) => state.restaurant);
   const cart = useSelector((state) => state.cart);
 
-  const [activePaymentMethods, setActivePaymentMethods] = useState([]);
+  const [paymentMethods, setPaymentMethods] = useState([]);
+  const [selectedPaymentMethod, setSelectedPaymentMethod] = useState(null);
+
+  useEffect(() => {
+    if (cart.orderType === 0) {
+      const paymentMethodsForDelivery =
+        restaurant.orderSettings.paymentMethods.filter(
+          (paymentMethod) => paymentMethod.delivery === true
+        );
+      setPaymentMethods(paymentMethodsForDelivery);
+    } else if (cart.orderType === 1) {
+      const paymentMethodsForTakeAway =
+        restaurant.orderSettings.paymentMethods.filter(
+          (paymentMethod) => paymentMethod.takeAway === true
+        );
+      setPaymentMethods(paymentMethodsForTakeAway);
+    }
+  }, [cart.orderType]);
+
   const [form, setForm] = useState({
     adress: "",
     postCode: "",
@@ -26,104 +47,201 @@ export default function Checkout() {
 
   //validations forms
   const [validationErrors, setValidationErrors] = useState({
+    adress: "",
     postCode: "",
+    city: "",
+    firstname: "",
+    lastname: "",
     mail: "",
     phoneNumber: "",
+    paymentMethod: "",
   });
+
+  const [orderError, setOrderError] = useState(false);
 
   const checkIfInputContainsOnlyNumber = (input) => {
     const numericRegex = /^[0-9]+$/;
     return numericRegex.test(input);
   };
 
-  const postCodeValidation = (postcode) => {
-    if (checkIfInputContainsOnlyNumber(postcode) || !postcode) {
-      setValidationErrors({
-        ...validationErrors,
-        postCode: "",
-      });
+  const adressValidation = (adress) => {
+    !adress
+      ? setValidationErrors((previous) => ({
+          ...previous,
+          adress: "L'adresse est obligatoire",
+        }))
+      : setValidationErrors((previous) => ({ ...previous, adress: "" }));
+  };
+
+  const postCodeValidation = (postCode) => {
+    if (!postCode) {
+      setValidationErrors((previous) => ({
+        ...previous,
+        postCode: "Le code postal est obligatoire",
+      }));
+    } else if (!checkIfInputContainsOnlyNumber(postCode)) {
+      setValidationErrors((previous) => ({
+        ...previous,
+        postCode: "Le code postal doit être uniquement constitué de chiffres",
+      }));
     } else {
-      setValidationErrors({
-        ...validationErrors,
-        postCode:
-          "Le code postal doit être consituté uniquement constitué de chiffres",
-      });
+      setValidationErrors((previous) => ({
+        ...previous,
+        postCode: "",
+      }));
     }
   };
 
-  const phoneNumberValidation = (phoneNumber) => {
-    if (checkIfInputContainsOnlyNumber(phoneNumber) || !phoneNumber) {
-      setValidationErrors({
-        ...validationErrors,
-        phoneNumber: "",
-      });
-    } else {
-      setValidationErrors({
-        ...validationErrors,
-        phoneNumber:
-          "Le numéro de téléphone doit être uniquement consituté de chiffres",
-      });
-    }
+  const cityValidation = (city) => {
+    !city
+      ? setValidationErrors((previous) => ({
+          ...previous,
+          city: "La ville est obligatoire",
+        }))
+      : setValidationErrors((previous) => ({ ...previous, city: "" }));
+  };
+
+  const firstnameValidation = (firstname) => {
+    !firstname
+      ? setValidationErrors((previous) => ({
+          ...previous,
+          firstname: "Le prénom est obligatoire",
+        }))
+      : setValidationErrors((previous) => ({ ...previous, firstname: "" }));
+  };
+
+  const lastnameValidation = (lastname) => {
+    !lastname
+      ? setValidationErrors((previous) => ({
+          ...previous,
+          lastname: "Le nom est obligatoire",
+        }))
+      : setValidationErrors((previous) => ({ ...previous, lastname: "" }));
   };
 
   const mailValidation = (mail) => {
-    if (mail) {
+    if (!mail) {
+      setValidationErrors((previous) => ({
+        ...previous,
+        mail: "L'adresse mail est obligatoire",
+      }));
+    } else {
       const mailRegex = /^[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,4}$/;
       if (mailRegex.test(mail) || !mail) {
-        setValidationErrors({
-          ...validationErrors,
+        setValidationErrors((previous) => ({
+          ...previous,
           mail: "",
-        });
+        }));
       } else {
-        setValidationErrors({
-          ...validationErrors,
+        setValidationErrors((previous) => ({
+          ...previous,
           mail: "L'adresse mail entrée n'est pas valide",
-        });
+        }));
       }
     }
   };
 
-  const paymentMethod = useState(null);
-
-  useEffect(() => {
-    if (cart.orderType === 0) {
-      const paymentMethodsForDelivery =
-        restaurant.orderSettings.paymentMethods.filter(
-          (paymentMethod) => paymentMethod.delivery === true
-        );
-      setActivePaymentMethods(paymentMethodsForDelivery);
-    } else if (cart.orderType === 1) {
-      const paymentMethodsForTakeAway =
-        restaurant.orderSettings.paymentMethods.filter(
-          (paymentMethod) => paymentMethod.takeAway === true
-        );
-      setActivePaymentMethods(paymentMethodsForTakeAway);
+  const phoneNumberValidation = (phoneNumber) => {
+    if (!phoneNumber) {
+      setValidationErrors((previous) => ({
+        ...previous,
+        phoneNumber: "Le numéro de téléphone est obligatoire",
+      }));
+    } else if (!checkIfInputContainsOnlyNumber(phoneNumber)) {
+      setValidationErrors((previous) => ({
+        ...previous,
+        phoneNumber:
+          "Le numéro de téléphone doit être uniquement constitué de chiffres",
+      }));
+    } else {
+      setValidationErrors((previous) => ({
+        ...previous,
+        phoneNumber: "",
+      }));
     }
-  }, [cart.orderType]);
+  };
+
+  const handleConfirmOrder = () => {
+    //display errors on submit and not only onBlur
+    adressValidation(form.adress);
+    postCodeValidation(form.postCode);
+    cityValidation(form.city);
+    firstnameValidation(form.firstname);
+    lastnameValidation(form.lastname);
+    mailValidation(form.mail);
+    phoneNumberValidation(form.phoneNumber);
+
+    if (!selectedPaymentMethod) {
+      setValidationErrors((previous) => ({
+        ...previous,
+        paymentMethod: "Aucun moyen de paiement sélectionné",
+      }));
+    } else {
+      setValidationErrors((previous) => ({
+        ...previous,
+        paymentMethod: "",
+      }));
+    }
+
+    if (
+      Object.values(validationErrors).every((value) => value === "") &&
+      selectedPaymentMethod
+    ) {
+      setOrderError(false);
+      router.push("/orderConfirmation", { scroll: false });
+    } else {
+      setOrderError(true);
+    }
+  };
 
   return (
-    <div className="h-screen relative flex flex-row overflow-y-auto">
-      <div className="flex flex-col grow items-start justify-start px-12 space-y-10">
+    <div className="relative flex flex-row">
+      <div className="flex flex-col items-start justify-start px-12 space-y-10 mb-10">
         <RestaurantLogo path={`images/${restaurant.uniqueValue}/logo.png`} />
         <div className="space-y-4">
           <h2 className="font-bold text-xl">Informations de commande</h2>
           <OrderTabBtn />
 
-          <FormInput label="Adresse" id="adress" />
+          <FormInput
+            label="Adresse"
+            id="adress"
+            onChange={(input) =>
+              setForm({
+                ...form,
+                adress: input,
+              })
+            }
+            value={form.adress}
+            validationFunction={adressValidation}
+            validationError={validationErrors.adress}
+          />
           <div className="flex flex-row space-x-4">
             <FormInput
               label="Code postal"
               id="postcode"
-              onChange={(e) =>
+              onChange={(input) =>
                 setForm({
                   ...form,
-                  postCode: e.target.value,
+                  postCode: input,
                 })
               }
+              value={form.postCode}
               validationFunction={postCodeValidation}
               validationError={validationErrors.postCode}
             />
-            <FormInput label="Ville" id="city" />
+            <FormInput
+              label="Ville"
+              id="city"
+              onChange={(input) =>
+                setForm({
+                  ...form,
+                  city: input,
+                })
+              }
+              value={form.city}
+              validationFunction={cityValidation}
+              validationError={validationErrors.city}
+            />
           </div>
           {cart.orderType === 0 ? (
             <h3 className="font-bold text-lg">
@@ -145,13 +263,15 @@ export default function Checkout() {
           <h2 className="font-bold text-xl">Moyen de paiement</h2>
           <fieldset className="mt-4">
             <div className="space-y-4 sm:flex sm:items-center sm:space-x-10 sm:space-y-0">
-              {activePaymentMethods.map((paymentMethod, i) => (
+              {paymentMethods.map((paymentMethod, i) => (
                 <div key={i} className="flex items-center">
                   <input
                     id={i}
                     name="payment-method"
                     type="radio"
                     className="h-4 w-4 border-gray-300 text-primary focus:ring-primary"
+                    onChange={() => setSelectedPaymentMethod(paymentMethods[i])}
+                    value={paymentMethod}
                   />
                   <label
                     htmlFor={paymentMethod.value}
@@ -164,42 +284,81 @@ export default function Checkout() {
             </div>
           </fieldset>
         </div>
+        {validationErrors.paymentMethod && (
+          <p className="text-error-danger">{validationErrors.paymentMethod}</p>
+        )}
 
         <div>
           <h2 className="font-bold text-xl">Informations personnelles</h2>
           <div className="flex flex-row space-x-4">
-            <FormInput label="Prénom" id="firstname" />
-            <FormInput label="Nom" id="lastname" />
+            <FormInput
+              label="Prénom"
+              id="firstname"
+              onChange={(input) =>
+                setForm({
+                  ...form,
+                  firstname: input,
+                })
+              }
+              value={form.firstname}
+              validationFunction={firstnameValidation}
+              validationError={validationErrors.firstname}
+            />
+            <FormInput
+              label="Nom"
+              id="lastname"
+              onChange={(input) =>
+                setForm({
+                  ...form,
+                  lastname: input,
+                })
+              }
+              value={form.lastname}
+              validationFunction={lastnameValidation}
+              validationError={validationErrors.lastname}
+            />
           </div>
 
           <div className="flex flex-row space-x-4">
             <FormInput
-              label="mail"
+              label="Adresse mail"
               id="mail"
               placeholder="you@example.com"
-              onChange={(e) =>
+              onChange={(input) =>
                 setForm({
                   ...form,
-                  mail: e.target.value,
+                  mail: input,
                 })
               }
+              value={form.mail}
               validationFunction={mailValidation}
               validationError={validationErrors.mail}
             />
             <FormInput
               label="N° de téléphone"
               id="phonenumber"
-              onChange={(e) =>
+              onChange={(input) =>
                 setForm({
                   ...form,
-                  phoneNumber: e.target.value,
+                  phoneNumber: input,
                 })
               }
+              value={form.phoneNumber}
               validationFunction={phoneNumberValidation}
               validationError={validationErrors.phoneNumber}
             />
           </div>
         </div>
+        {orderError && (
+          <p className="text-error-danger self-center">
+            Un ou plusieurs des champs ci-dessus sont invalides
+          </p>
+        )}
+        <DefaultBtn
+          value={"Confirmer la commande"}
+          className="w-72 h-12 text-xl font-bold bg-success hover:opacity-90 self-center"
+          onClick={handleConfirmOrder}
+        />
       </div>
       <Cart variant="checkout" />
     </div>
--- src/components/ui/FormInput.js
+++ src/components/ui/FormInput.js
@@ -1,63 +1,46 @@
 "use client";
 import { ExclamationCircleIcon } from "@heroicons/react/20/solid";
-import { useState } from "react";
 export default function FormInput({
   label,
   placeholder,
   id,
+  value,
   onChange,
   validationFunction,
   validationError,
 }) {
-  const [isEmpty, setEmpty] = useState(false);
-  const checkEmpty = (string) => {
-    if (string === "") {
-      setEmpty(true);
-    } else {
-      setEmpty(false);
-    }
-  };
-
-  const onBlur = (value) => {
-    checkEmpty(value);
-    validationFunction && validationFunction(value);
-  };
-
   return (
     <div>
-      <label
-        htmlFor={id}
-        className="ml-px block pl-4 text-sm font-medium leading-6 text-gray-900"
-      >
-        {label}
-      </label>
-      <div className="mt-2 relative">
-        <input
-          type="text"
-          name={id}
-          id={id}
-          placeholder={placeholder}
-          className={`${
-            validationError || isEmpty ? "ring-error-danger" : "ring-gray-300"
-          } block w-full rounded-xl border-0 px-4 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-primary sm:text-sm sm:leading-6`}
-          onChange={onChange}
-          onBlur={(e) => onBlur(e.target.value)}
-        />
-        {(isEmpty || validationError) && (
-          <div className="pointer-events-none absolute inset-y-0 right-0 flex items-center pr-3">
-            <ExclamationCircleIcon
-              className="h-5 w-5 text-red-500"
-              aria-hidden="true"
-            />
-          </div>
-        )}
+      <div>
+        <label
+          htmlFor={id}
+          className="ml-px block pl-4 text-sm font-medium leading-6 text-gray-900"
+        >
+          {label}
+        </label>
+        <div className="mt-2 relative">
+          <input
+            type="text"
+            name={id}
+            id={id}
+            value={value}
+            placeholder={placeholder}
+            className={`${
+              validationError ? "ring-error-danger" : "ring-gray-300"
+            } block w-full rounded-xl border-0 px-4 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-primary sm:text-sm sm:leading-6`}
+            onChange={(e) => onChange(e.target.value)}
+            onBlur={(e) => validationFunction(e.target.value)}
+          />
+          {validationError && (
+            <div className="pointer-events-none absolute inset-y-0 right-0 flex items-center pr-3">
+              <ExclamationCircleIcon
+                className="h-5 w-5 text-red-500"
+                aria-hidden="true"
+              />
+            </div>
+          )}
+        </div>
       </div>
-
-      {isEmpty && (
-        <p className="mt-2 text-sm text-error-danger">
-          Veuillez remplir ce champ
-        </p>
-      )}
       {validationError && (
         <p className="mt-2 text-sm text-error-danger">{validationError}</p>
       )}
