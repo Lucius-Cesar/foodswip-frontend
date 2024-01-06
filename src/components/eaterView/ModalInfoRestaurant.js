b06e84c320a6de8fa868e51b26688d1fee788888
lucius-César <luciendelgrange5@gmail.com>
2024-01-06 15:03:18 +0100

    dvh instead of vh for modalFood modalInfoRestaurant

--- src/components/eaterView/ModalInfoRestaurant.js
+++ src/components/eaterView/ModalInfoRestaurant.js
@@ -49,7 +49,7 @@ export default function ModalInfoRestaurant({ open, setOpen }) {
               leaveFrom="opacity-100 translate-y-0 sm:scale-100"
               leaveTo="opacity-0 translate-y-4 sm:translate-y-0 sm:scale-95"
             >
-              <Dialog.Panel className="flex flex-col relative w-screen h-screen  sm:w-7/12 sm:h-auto sm:max-h-full transform overflow-hidden rounded-lg bg-magnolia text-left shadow-xl transition-all  px-4 pb-4 pt-5">
+              <Dialog.Panel className="flex flex-col relative w-screen h-dvh  sm:w-7/12 sm:h-auto sm:max-h-full transform overflow-hidden rounded-lg bg-magnolia text-left shadow-xl transition-all  px-4 pb-4 pt-5">
                 <div className="flex fex-row w-full justify-between pb-4">
                   <h2>Informations du restaurant</h2>
                   <button
