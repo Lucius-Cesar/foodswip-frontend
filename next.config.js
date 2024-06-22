27f96e61852d8801634ebd623df8a9542e923846
Lucien <luciendelgrange5@gmail.com>
2024-06-22 20:11:42 +0200

    remove next/pwa adapt nextconfig

--- next.config.js
+++ next.config.js
@@ -11,17 +11,4 @@ const nextConfig = {
   },
 };
 
-const runtimeCaching = require("next-pwa/cache");
-const withPWA = require("next-pwa")({
-  dest: "public",
-  register: true,
-  skipWaiting: true,
-  runtimeCaching,
-});
-
-module.exports = withPWA({
-  // other congigs
-  reactStrictMode: false,
-});
-
 module.exports = nextConfig;
