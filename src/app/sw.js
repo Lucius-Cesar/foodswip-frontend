31a7892ce0595879cad9a84cd2398e9d392f3f7f
Lucien <luciendelgrange5@gmail.com>
2024-06-23 11:50:09 +0200

    vercel manifest update

--- src/app/sw.js
+++ src/app/sw.js
@@ -1,6 +1,7 @@
 import { defaultCache } from "@serwist/next/worker";
 import { Serwist } from "serwist";
 
+const self = self;
 const serwist = new Serwist({
   precacheEntries: self.__SW_MANIFEST,
   skipWaiting: true,
