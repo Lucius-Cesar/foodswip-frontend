c89e5174624bc02e496cb71311ef3f7af420a670
Lucien <luciendelgrange5@gmail.com>
2024-07-12 22:18:59 +0200

    menu-generator update

--- src/app/menu-generator/page.js
+++ src/app/menu-generator/page.js
@@ -9,11 +9,14 @@ import { useEffect, useRef, useState } from "react";
 const page = () => {
   const [slug, setSlug] = useState(null);
   const [categoriesToDelete, setCategoriesToDelete] = useState("");
-  const [maxFoodPerCategory, setMaxFoodPerCategory] = useState(null);
+  const [foodsToDelete, setfoodsToDelete] = useState("");
+
   const [menu, setMenu] = useState(null);
   const [menuImgUrl, setMenuImgUrl] = useState(null);
   const [loading, setLoading] = useState(false);
-  const [disposition, setDisposition] = useState("optimizeSpace");
+  const [textSize, setTextSize] = useState("medium");
+  const [cols, setCols] = useState(3);
+  const [textAlignment, setTextAlignment] = useState("start");
   const menuImgRef = useRef(null);
   const menuDownloadRef = useRef(null);
 
@@ -50,8 +53,12 @@ const page = () => {
     setMenuImgUrl(blobUrl);
   };
 
-  const handleDispositionChange = (event) => {
-    setDisposition(event.target.value);
+  const handleTextSizeChange = (event) => {
+    setTextSize(event.target.value);
+  };
+
+  const handleTextAlignmentChange = (event) => {
+    setTextAlignment(event.target.value);
   };
 
   const handleGenerateAndDownloadMenu = async (e) => {
@@ -83,45 +90,100 @@ const page = () => {
               onChange={(input) => setCategoriesToDelete(input)}
             ></FormInput>
           </div>
+
           <div className="w-96">
-            <label className="font-medium">
-              Maximum de plats par catégories (ne rien mettre par défaut){" "}
-            </label>
-            <InputNumber
-              onChange={(input) => setMaxFoodPerCategory(input)}
-            ></InputNumber>
+            <FormInput
+              label="Plats à supprimer (séparés par ;)"
+              placeholder="Tagliatelles aux fruits de mer;Gnocchi à la sauce pesto;"
+              onChange={(input) => setfoodsToDelete(input)}
+            ></FormInput>
+          </div>
+          <div>
+            <label className="font-medium">Taille du texte</label>
+            <div className="flex flex-row gap-2">
+              <input
+                type="radio"
+                id="small"
+                name="textSize"
+                className={`h-6 w-6 sm:h-4 sm:w-4 rounded border-gray-300 text-primary focus:ring-primary`}
+                value="small"
+                checked={textSize === "small"}
+                onChange={handleTextSizeChange}
+              />
+              <label htmlFor="small">Petit</label>
+            </div>
+            <div className="flex flex-row gap-2">
+              <input
+                type="radio"
+                id="medium"
+                name="textSize"
+                className={`h-6 w-6 sm:h-4 sm:w-4 rounded border-gray-300 text-primary focus:ring-primary`}
+                value="medium"
+                checked={textSize === "medium"}
+                onChange={handleTextSizeChange}
+              />
+              <label htmlFor="medium">Moyen</label>
+            </div>
+            <div className="flex flex-row gap-2">
+              <input
+                type="radio"
+                className={`h-6 w-6 sm:h-4 sm:w-4 rounded border-gray-300 text-primary focus:ring-primary`}
+                id="large"
+                name="textSize"
+                value="large"
+                checked={textSize === "large"}
+                onChange={handleTextSizeChange}
+              />
+              <label htmlFor="large">Grand</label>
+            </div>
+            <div className="flex flex-row gap-2">
+              <input
+                type="radio"
+                className={`h-6 w-6 sm:h-4 sm:w-4 rounded border-gray-300 text-primary focus:ring-primary`}
+                id="extra-large"
+                name="textSize"
+                value="extra-large"
+                checked={textSize === "extra-large"}
+                onChange={handleTextSizeChange}
+              />
+              <label htmlFor="extra-large">XL</label>
+            </div>
           </div>
 
           <div>
-            <label className="font-medium">Disposition du menu</label>
+            <label className="font-medium">Alignement du texte</label>
             <div className="flex flex-row gap-2">
               <input
                 type="radio"
-                id="optimizeSpace"
+                id="start"
+                name="textAlignment"
                 className={`h-6 w-6 sm:h-4 sm:w-4 rounded border-gray-300 text-primary focus:ring-primary`}
-                name="disposition"
-                value="optimizeSpace"
-                checked={disposition === "optimizeSpace"}
-                onChange={handleDispositionChange}
+                value="start"
+                checked={textAlignment === "start"}
+                onChange={handleTextAlignmentChange}
               />
-              <label htmlFor="optimizeSpace">
-                Optimiser l'espace (recommandés pour les grands menus)
-              </label>
-            </div>
-            <div className="flex flex-row gap-2">
+              <label htmlFor="center">Début</label>
               <input
                 type="radio"
+                id="center"
+                name="textAlignment"
                 className={`h-6 w-6 sm:h-4 sm:w-4 rounded border-gray-300 text-primary focus:ring-primary`}
-                id="symmetric"
-                name="disposition"
-                value="symmetric"
-                checked={disposition === "symmetric"}
-                onChange={handleDispositionChange}
+                value="center"
+                checked={textAlignment === "center"}
+                onChange={handleTextAlignmentChange}
               />
-              <label htmlFor="symmetric">
-                Symétrique (recommandés pour les petits menus)
-              </label>
+              <label htmlFor="center">Centré</label>
             </div>
+          </div>
+          <div className="w-44">
+            <label className="font-medium">Nombre de colonnes</label>
+            <InputNumber
+              value={cols}
+              onChange={(input) => {
+                if (!input) setCols(1);
+                else setCols(input);
+              }}
+            ></InputNumber>
           </div>
           <div>
             <DefaultBtn
@@ -135,17 +197,17 @@ const page = () => {
             ref={menuDownloadRef}
             href={menuImgUrl ? menuImgUrl : "#"}
             download={`menu_${slug}.png`}
-          >
-            {" "}
-          </a>
+          ></a>
         </form>
         {menu && (
-          <div className="h-[1690px] w-[1307px]" ref={menuImgRef}>
+          <div className="h-[1800px] w-[1250px]" ref={menuImgRef}>
             <MenuForFlyers
               menu={menu}
-              maxFoodPerCategory={maxFoodPerCategory}
+              textSize={textSize}
               categoriesToDelete={categoriesToDelete}
-              disposition={disposition}
+              foodsToDelete={foodsToDelete}
+              cols={cols}
+              textAlignment={textAlignment}
             />
           </div>
         )}
--- src/components/menu-generator/MenuForFlyers.js
+++ src/components/menu-generator/MenuForFlyers.js
@@ -1,139 +1,134 @@
 const MenuForFlyers = ({
   menu,
   categoriesToDelete,
-  maxFoodPerCategory,
-  disposition,
+  foodsToDelete,
+  textSize,
+  cols,
+  textAlignment,
 }) => {
-  let menuColumns = [[], [], []];
+  const textSizes = {
+    title:
+      textSize === "small"
+        ? "text-3xl"
+        : textSize === "medium"
+        ? "text-4xl"
+        : textSize === "large"
+        ? "text-5xl"
+        : textSize === "extra-large"
+        ? "text-6xl"
+        : null,
+    food:
+      textSize === "small"
+        ? "text-lg"
+        : textSize === "medium"
+        ? "text-xl"
+        : textSize === "large"
+        ? "text-2xl"
+        : textSize === "extra-large"
+        ? "text-3xl"
+        : null,
+    description:
+      textSize === "small"
+        ? "text-normal"
+        : textSize === "medium"
+        ? "text-lg"
+        : textSize === "large"
+        ? "text-xl"
+        : textSize === "extra-large"
+        ? "text-2xl"
+        : null,
+  };
 
-  // Supprimer les catégories à supprimer
-  categoriesToDelete = categoriesToDelete.split(";");
-  menu = menu.filter(
-    (category) =>
-      !categoriesToDelete
-        .map((item) => item.toLowerCase())
-        .includes(category.title.toLowerCase())
+  const marginAfterTitle =
+    textSize === "small"
+      ? "mb-2"
+      : textSize === "medium"
+      ? "mb-3"
+      : textSize === "large"
+      ? "mb-6"
+      : textSize === "extra-large"
+      ? "mb-8"
+      : null;
+
+  categoriesToDelete = categoriesToDelete
+    .split(";")
+    .map((category) => category.toLowerCase());
+
+  // Assuming foodsToDelete is a string of food items separated by semicolons
+  foodsToDelete = foodsToDelete.split(";").map((food) => food.toLowerCase());
+
+  menu = menu.filter((category) => {
+    // Filter out categories that are in categoriesToDelete
+    if (categoriesToDelete.includes(category.title.toLowerCase())) {
+      return false;
+    }
+
+    // Further filter out individual foods within each category that are in foodsToDelete
+    category.foods = category.foods.filter((food) => {
+      return !foodsToDelete.includes(food.value.toLowerCase());
+    });
+
+    return true;
+  });
+
+  let menuColumns = Array.from({ length: cols }, () => []);
+  let totalFoods = menu.reduce(
+    (acc, category) => acc + category.foods.length,
+    0
   );
+  const idealFoodsPerCol = Math.ceil(totalFoods / cols);
 
-  if (maxFoodPerCategory) {
-    menu = menu.map((category) => ({
-      ...category,
-      foods: category.foods.slice(0, maxFoodPerCategory),
-    }));
-  }
-  if (disposition === "optimizeSpace") {
-    const cols = 3;
-    menuColumns = Array.from({ length: cols }, () => []);
-    let totalFoods = menu.reduce(
-      (acc, category) => acc + category.foods.length,
-      0
-    );
-    const idealFoodsPerCol = Math.ceil(totalFoods / cols);
-
-    menu.forEach((category) => {
-      // Trouver la colonne avec le moins de plats
-      const columnIndex = menuColumns.reduce(
-        (minIndex, currentCol, currentIndex, array) => {
-          const currentColFoods = array[minIndex].reduce(
-            (acc, cat) => acc + cat.foods.length,
-            0
-          );
-          const newColFoods = currentCol.reduce(
-            (acc, cat) => acc + cat.foods.length,
-            0
-          );
-          return newColFoods < currentColFoods ? currentIndex : minIndex;
-        },
-        0
-      );
-
-      menuColumns[columnIndex].push(category);
-    });
-  } else if (disposition === "symmetric") {
-    const totalFoods = menu.reduce(
-      (acc, category) => acc + category.foods.length,
+  menu.forEach((category) => {
+    // Trouver la colonne avec le moins de plats
+    const columnIndex = menuColumns.reduce(
+      (minIndex, currentCol, currentIndex, array) => {
+        const currentColFoods = array[minIndex].reduce(
+          (acc, cat) => acc + cat.foods.length,
+          0
+        );
+        const newColFoods = currentCol.reduce(
+          (acc, cat) => acc + cat.foods.length,
+          0
+        );
+        return newColFoods < currentColFoods ? currentIndex : minIndex;
+      },
       0
     );
 
-    const cols = 3;
-    // Calculer le nombre idéal de plats par colonne
-    const idealFoodsPerCol = Math.ceil(totalFoods / cols);
+    menuColumns[columnIndex].push(category);
+  });
 
-    let currentColFoods = 0; // Compteur pour le nombre de plats dans la colonne actuelle
-
-    // Répartir les catégories entre les colonnes
-    menu.forEach((category) => {
-      if (
-        currentColFoods + category.foods.length > idealFoodsPerCol &&
-        menuColumns[0].length > menuColumns[1].length
-      ) {
-        // Passer à la colonne suivante si l'ajout de cette catégorie dépasse le nombre idéal et la colonne actuelle n'est pas la dernière
-        menuColumns[1].push(category);
-        currentColFoods = category.foods.length;
-      } else if (
-        currentColFoods + category.foods.length > idealFoodsPerCol &&
-        menuColumns[1].length > menuColumns[2].length
-      ) {
-        menuColumns[2].push(category);
-        currentColFoods = category.foods.length;
-      } else {
-        // Ajouter la catégorie à la colonne actuelle
-        menuColumns[0].push(category);
-        currentColFoods += category.foods.length;
-      }
-    });
-  }
-  // Limiter le nombre de plats par catégorie si nécessaire
-
-  if (disposition === "optimizeSpace") {
-    return (
-      <div className="flex justify-between px-4 h-full w-full">
-        {menuColumns.map((column, idx) => (
-          <div key={idx} className="w-1/3 px-2">
-            {column.map((category) => (
-              <div key={category.title} className="mb-8">
-                <h1 className="text-primary font-bold mb-2">
-                  {category.title}
-                </h1>
-                {category.foods.map((food) => (
-                  <div key={food.value} className="mb-4">
-                    <h4 className="text-md font-semibold">
-                      {food.value} - {food.price} €
-                    </h4>
-                    <p className="text-sm">{food.description}</p>
-                  </div>
-                ))}
-              </div>
-            ))}
-          </div>
-        ))}
-      </div>
-    );
-  } else if (disposition === "symmetric") {
-    return (
-      <div className="flex justify-between px-4 h-full w-full">
-        {menuColumns.map((column, idx) => (
-          <div key={idx} className="w-1/3 px-2">
-            {column.map((category) => (
-              <div key={category.title} className="mb-8">
-                <h1 className="text-primary font-bold mb-2">
-                  {category.title}
-                </h1>
-                {category.foods.map((food) => (
-                  <div key={food.value} className="mb-4">
-                    <h4 className="text-md font-semibold">
-                      {food.value} - {food.price} €
-                    </h4>
-                    <p className="text-sm">{food.description}</p>
-                  </div>
-                ))}
-              </div>
-            ))}
-          </div>
-        ))}
-      </div>
-    );
-  }
+  return (
+    <div
+      className={`flex ${cols === 1 ? "flex-col" : ""} justify-between ${
+        textAlignment === "center" ? "text-center" : ""
+      } px-4 h-full w-full `}
+    >
+      {menuColumns.map((column, idx) => (
+        <div key={idx} className={`w-1/${cols} px-2`}>
+          {column.map((category) => (
+            <div key={category.title} className="mb-8">
+              <p
+                className={`${textSizes.title} text-primary font-bold ${marginAfterTitle}`}
+              >
+                {category.title}
+              </p>
+              {category.foods.map((food) => (
+                <div key={food.value} className="mb-4">
+                  <p className={`${textSizes.food} font-semibold`}>
+                    {food.value} - {food.price} €
+                  </p>
+                  <p className={`${textSizes.description}`}>
+                    {food.description}
+                  </p>
+                </div>
+              ))}
+            </div>
+          ))}
+        </div>
+      ))}
+    </div>
+  );
 };
 
 export default MenuForFlyers;
